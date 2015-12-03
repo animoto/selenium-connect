417b32a1737845e8734e0ba435588124f5448eb3
Jay Sirju <Jay.Sirju@animoto.com>
2015-12-03 15:53:38 -0500

    Increase resolution used for Browserstack tests

--- lib/selenium_connect/runners/browserstack.rb
+++ lib/selenium_connect/runners/browserstack.rb
@@ -23,6 +23,7 @@ class SeleniumConnect
       def init_browser
         config.browserstack_opts[:'browserstack.debug'] = 'true'
         config.browserstack_opts[:'browserstack.video'] = 'true'
+        config.browserstack_opts[:resolution] = '1920x1080'
         driver = Selenium::WebDriver.for(
           :remote,
           url: "http://#{config.browserstack_username}:#{config.browserstack_api_key}@hub.browserstack.com/wd/hub",
